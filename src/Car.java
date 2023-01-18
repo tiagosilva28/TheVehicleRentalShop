63f2791bccae0c2f35909559d6961d812eabad77
GutoFaria365 <augusto.filipefaria@gmail.com>
2023-01-18 15:24:37 +0000

    car trip calculation

--- src/Car.java
+++ src/Car.java
@@ -8,7 +8,10 @@ public class Car extends Vehicle
     @Override
     public void trip(int distance, int time) {
         if (this.getGasConsumption() * this.getTank() >= distance) {
+            System.out.println("-------------CARS-----------");
             System.out.println("Have a good trip");
+            this.setTank((int) (this.getTank() - this.getGasConsumption() * distance));
+            System.out.println(this.getTank());
         } else
         System.out.println("Your Car Trip is too long");
     }
--- src/Motocycle.java
+++ src/Motocycle.java
@@ -10,10 +10,11 @@ public class Motocycle extends Vehicle{
     @Override
     public void trip(int distance, int time) {
         if (distance>80){
+            System.out.println("-------------BIKES-----------");
             System.out.println("Your Trip is too long");
             return ;
         }
-        System.out.println(this.getTank());
+        //System.out.println(this.getTank());
 
     }
 }
