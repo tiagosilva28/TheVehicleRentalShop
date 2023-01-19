691c03c5d11310db6867c8f5655f01b6b9540fc4
tiagosilva28 <tiagosilva28@live.com.pt>
2023-01-19 10:48:03 +0000

    implementation of plates

--- src/Shop.java
+++ src/Shop.java
@@ -13,7 +13,7 @@ public class Shop {
         if (cars.isFree()){
             System.out.println("The car " + cars.getModelName() + " is ready for you");
             cars.setFree(false);
-            return new Car(cars.getModelName(), cars.getGasConsumption());
+            return new Car(cars.getModelName(), cars.getGasConsumption(), cars.getPlate());
         } else System.out.println("The car you want is not available!");
         return null;
     }
@@ -22,15 +22,15 @@ public class Shop {
         if (bike.isFree()){
             System.out.println("The bike " + bike.getModelName() + " is ready for you");
             bike.setFree(false);
-            return new Motocycle(bike.getModelName(), bike.getGasConsumption());
+            return new Motocycle(bike.getModelName(), bike.getGasConsumption(), bike.getPlate());
         } else System.out.println("The bike is not available!");
         return null;
     }
 
-    public void returnCar(Car myCar, CarsAvailable cars){
+    public void returnCar(Car myCar){
         for (int i = 0; i < CarsAvailable.values().length; i++) {
-            if (myCar.getPlate() == CarsAvailable.valueOf(cars[i].getPlate())) {
-                System.out.println(myCar.getPlate());
+            if (myCar.getPlate() == CarsAvailable.BMW.getPlate() ) {
+                System.out.println("Equal Plate");
             }
 
         }
